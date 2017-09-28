33f8a8c4095ef0cbff47e94474b1f6477ec71972
EgorMikhailov <31589543+EgorMikhailov@users.noreply.github.com>
2017-09-28 23:52:33 +0300

    Add files via upload

---
+++ hw03_2b.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int i, N;
+    scanf("%i", &N);
+    int flag = 0;
+    for (i = 0; i < N; ++i){
+        if (flag != getppid())
+        {
+            flag = getppid();
+            pid_t pid = fork();
+            sleep(1);
+            if (pid == -1) printf("error"); else
+            if (pid == 0) printf("process #%i created\n", i+1);
+            else {
+                wait(); 
+                sleep(1);
+                printf("process #%i killed\n", i+1);
+            }
+        }
+    }
+    return 0;
+}
